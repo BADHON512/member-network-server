6f469b8e530f0249df901db503ff0f5ea1386d2e
badhon <mylaptop512@gmail.com>
2023-11-03 17:23:48 +0600

    all backend done member network

--- ConnectDB.js
+++ ConnectDB.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const ConnectDB = async () => {
   await mongoose
-    .connect("mongodb://127.0.0.1:27017/Member-Network")
+    .connect("mongodb+srv://member-network:badhon@cluster0.ukp4otc.mongodb.net/Member-Network")
     .then((data) => {
       console.log(`mongodb connected with server ${data.connection.host}`);
     });
 };
 
 module.exports = ConnectDB;
+
+
