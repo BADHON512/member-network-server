ed2f761121f7e6eb4f620ebc3258b717f8b79b1c
MUHAMMAD BADHON <93506264+BADHON512@users.noreply.github.com>
2023-11-03 18:12:16 +0600

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app= express()
 app.use(express.json({limit: '1mb'}))
 app.use(express.urlencoded({extended:false,limit: '1mb'}))
 app.use(cors({
-    origin: "https://member-network-server.vercel.app/",
+    origin: "https://member-network-server.vercel.app",
     credentials: true
 }));
 app.use(cookieParser())
